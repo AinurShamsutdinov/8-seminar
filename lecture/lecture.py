daaf7b90424d110ba1d64a4aa27d18f485ca0c33
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-18 01:37:50 +0300

    lecture

--- lecture/lecture.py
+++ lecture/lecture.py
@@ -134,21 +134,118 @@
 # print(c)
 # print(j)
 ###################################################################################
-import csv
-with open('biostats.csv', 'r', newline='') as f:
-    csv_file = csv.reader(f)
-    for line in csv_file:
-        print(line)
-    print(type(line))
+# import csv
+# with open('biostats.csv', 'r', newline='') as f:
+#     csv_file = csv.reader(f)
+#     for line in csv_file:
+#         print(line)
+#     print(type(line))
 ###################################################################################
+# import csv
+# with open('biostats_tab.csv', 'r', newline='') as f:
+#     csv_file = csv.reader(f, dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+#     for line in csv_file:
+#         print(line)
+#     print(type(line))
 ###################################################################################
+# import csv
+# with (
+#         open('biostats_tab.csv', 'r', newline='') as f_read,
+#         open('new_biostats.csv', 'w', newline='', encoding='utf-8') as f_write):
+#             csv_read = csv.reader(f_read, dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+#             csv_write = csv.writer(f_write, dialect='excel', delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#             all_data = []
+#             for i, line in enumerate(csv_read):
+#                 if i == 0:
+#                     csv_write.writerow(line)
+#                 else:
+#                     line[2] += 1
+#                     for j in range(2, 4 + 1):
+#                         line[j] = int(line[j])
+#                     all_data.append(line)
+#             csv_write.writerows(all_data)
 ###################################################################################
+# import csv
+# with open('biostats_tab.csv', 'r', newline='') as f:
+#     csv_file = csv.DictReader(f, fieldnames=["name", "sex", "age", "height", "weight", "office"], restkey="new", restval="Main Office", dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+#     for line in csv_file:
+#         print(f'{line = }')
+#         print(f'{line["name"] = }\t{line["age"] = }')
 ###################################################################################
+# import csv
+# from typing import Iterator
+# with (
+#     open('biostats_tab.csv', 'r', newline='') as f_read,
+#     open('biostats_new.csv', 'w', newline='', encoding='utf-8') as f_write):
+#         csv_read: Iterator[dict] = csv.DictReader(f_read, fieldnames=["name", "sex", "age", "height", "weight", "office"], restval="Main Office", dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+#         csv_write = csv.DictWriter(f_write, fieldnames=["id", "name", "office", "sex", "age", "height", "weight"], dialect='excel-tab', quoting=csv.QUOTE_ALL)
+#         csv_write.writeheader()
+#         all_data = []
+#         for i, dict_row in enumerate(csv_read):
+#             if i != 0:
+#                 dict_row['id'] = i
+#                 dict_row['age'] += 1
+#                 all_data.append(dict_row)
+#         csv_write.writerows(all_data)
 ###################################################################################
+# import pickle
+#
+# res = pickle.loads(b"cos\nsystem\n(S'echo Hello world!'\ntR.")
+# print(res)
 ###################################################################################
+# import pickle
+#
+# my_dict = {
+#     "first_name": "Джон",
+#     "last_name": "Смит",
+#     "hobbies": ["кузнечное дело", "программирование", "путешествия"],
+#     "age": 35,
+#     "children": [
+#         {
+#             "first_name": "Алиса",
+#             "age": 5
+#         },
+#         {
+#             "first_name": "Маруся",
+#             "age": 3
+#         }
+#     ]
+# }
+# print(my_dict)
+# res = pickle.dumps(my_dict, protocol=pickle.DEFAULT_PROTOCOL)
+# print(f'{res = }')
 ###################################################################################
+# import pickle
+#
+#
+# def func(a, b, c):
+#     return a + b + c
+#
+#
+# my_dict = {
+#     "numbers": [42, 4.1415, 7+3j],
+#     "functions": (func, sum, max),
+#     "others": {True, False, 'Hello world!'},
+# }
+# with open('my_dict.pickle', 'wb') as f:
+#     pickle.dump(my_dict, f)
 ###################################################################################
+# import pickle
+# data = b'\x80\x04\x95\xe3\x00\x00\x00\x00\x00\x00\x00}\x94(\x8c\nfirst_name\x94\x8c\x08\xd0\x94\xd0\xb6\xd0\xbe\xd0\xbd\x94\x8c\tlast_name\x94\x8c\x08\xd0\xa1\xd0\xbc\xd0\xb8\xd1\x82\x94\x8c\x07hobbies\x94]\x94(\x8c\x1b\xd0\xba\xd1\x83\xd0\xb7\xd0\xbd\xd0\xb5 \xd1\x87\xd0\xbd\xd0\xbe\xd0\xb5\xd0\xb4\xd0\xb5\xd0\xbb\xd0\xbe\x94\x8c\xd0\xbf\xd1\x80\xd0\xbe\xd0\xb3\xd1\x80\xd0\xb0\xd0\xbc\xd0\xbc\xd0\xb8\xd1\x80\xd0\xbe\xd0\xb2\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb5\x94\x8c\x16\xd0\xbf\xd1\x83\xd1\x82\xd0\xb5\xd1\x88\xd0\xb5\xd1\x81\xd1\x82\xd0\xb2\xd0\xb8\xd1\x8f\x94e\x8c\x03age\x94K#\x8c\x08children\x94]\x94(}\x94(h\x01\x8c\n\xd0\x90\xd0\xbb\xd0\xb8\xd1\x81\xd0\xb0\x94h\nK\x05u}\x94(h\x01\x8c\x0c\xd0\x9c\xd0\xb0\xd1\x80\xd1\x83\xd1\x81\xd1\x8f\x94h\nK\x03ueu.'
+# new_dict = pickle.loads(data)
+# print(f'{new_dict = }')
 ###################################################################################
+# import pickle
+#
+#
+# def func(a, b, c):
+#     return a * b * c
+#
+#
+# with open('my_dict.pickle', 'rb') as f:
+#     new_dict = pickle.load(f)
+# print(f'{new_dict = }')
+# print(f'{new_dict["functions"][0](2, 3, 4) = }')
 ###################################################################################
 ###################################################################################
 ###################################################################################
